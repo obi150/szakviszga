9f17147465080fc5c2188cc55300788058eec3f3
obi150 <161379869+obi150@users.noreply.github.com>
2025-02-09 22:16:34 +0200

    Add files via upload

--- test3/js/sketch.js
+++ test3/js/sketch.js
@@ -3,8 +3,6 @@ let angleX = 0, angleY = 0;
 let speed = 5;
 let clickStartX, clickStartY;
 
-let cir = new hitbox(100, [0, 0, -200])
-
 function calculateDistance(A, vect, O) {
     let newVect = [A[0] - O[0], A[1] - O[1], A[2] - O[2]];
     let t = (newVect[0] * vect[0] + newVect[1] * vect[1] + newVect[2] * vect[2]) / (vect[0] * vect[0] + vect[1] * vect[1] + vect[2] * vect[2]);
@@ -55,8 +53,6 @@ function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
 
     perspective(PI / 3, width / height, 0.1, 1000);
-
-    setUpChessBoard();
 }
 
 function draw() {
@@ -88,11 +84,31 @@ function draw() {
 
     camera(camX, camY, camZ, camX + sin(angleX) * cos(angleY), camY + sin(angleY), camZ + cos(angleX) * cos(angleY), 0, 1, 0);
 
-    let poz = cir.getPosition()
-    translate(poz[0], poz[1], poz[2]);
-    noStroke();
-    fill(cir.getColor());
-    sphere(cir.getRadius());
+    noFill();
+    stroke(0);
+    for (let i = 2; i < 10; i++) {
+        for (let j = 2; j < 10; j++) {
+            for (let k = 2; k < 10; k++) {
+                push();
+                translate(200 - 50 * (i - 2), 200 - 50 * (j - 2), 200 - 50 * (k - 2));
+                noFill();
+                stroke(0);
+                box(50);
+                if (chessBoard3D[i][j][k]) {
+                    if (chessBoard3D[i][j][k] == 13)
+                        fill(0, 0, 255);
+                    else if (chessBoard3D[i][j][k] < 7)
+                        fill(255, 255, 255);
+                    else
+                        fill(0, 0, 0);
+                    noStroke();
+                    sphere(25);
+                }
+                
+                pop();
+            }
+        }
+    }
 }
 
 function mouseDragged() {
@@ -107,8 +123,46 @@ function mousePressed() {
 function mouseReleased() {
     if (clickStartX == mouseX && clickStartY == mouseY) {
         let vect = createRay();
-        if (cir.isHit(vect, [camX, camY, camZ])) {
-            cir.changeColor([0, 255, 0]);
+        let pieceDistanceIndex = -1, pieceDistance = 999999;
+        let hitboxDistanceIndex = -1, hitboxDistance = 999999;
+        for (let i = 0; i < pieces.length; i++) {
+            if (pieces[i].isHit(vect, [camX, camY, camZ])) {
+                let newDis = Math.sqrt((pieces[i].getPosition()[0] - camX) * (pieces[i].getPosition()[0] - camX) +
+                    (pieces[i].getPosition()[1] - camY) * (pieces[i].getPosition()[1] - camY) +
+                    (pieces[i].getPosition()[2] - camZ) * (pieces[i].getPosition()[2] - camZ));
+                if (newDis < pieceDistance) {
+                    pieceDistance = newDis;
+                    pieceDistanceIndex = i;
+                }
+            }
         }
+        for (let i = 0; i < hitboxes.length; i++) {
+            if (hitboxes[i].isHit(vect, [camX, camY, camZ])) {
+                let newDis = Math.sqrt(hitboxes[i].getPosition()[0] * hitboxes[i].getPosition()[0] + hitboxes[i].getPosition()[1] * hitboxes[i].getPosition()[1] + hitboxes[i].getPosition()[2] * hitboxes[i].getPosition()[2]);
+                if (newDis < hitboxDistance) {
+                    hitboxDistance = newDis;
+                    hitboxDistanceIndex = i;
+                }
+            }
+        }
+        console.log(pieceDistance, hitboxDistance);
+        if (pieceDistanceIndex > -1) {
+            if (hitboxDistanceIndex > -1) {
+                if (pieceDistance < hitboxDistance) {
+                    currentPieceIndex = pieceDistanceIndex;
+                    createMoveOprionsHitboxesByID(pieces[pieceDistanceIndex].getId(), pieces[pieceDistanceIndex].getPosition());
+                }
+                else {
+                    createMoveOprionsHitboxesByID(hitboxes[hitboxDistanceIndex].getId(), hitboxes[hitboxDistanceIndex].getPosition());
+                }
+            }
+            else {
+                currentPieceIndex = pieceDistanceIndex;
+                createMoveOprionsHitboxesByID(pieces[pieceDistanceIndex].getId(), pieces[pieceDistanceIndex].getPosition());
+            }
+
+        }
+        else if (hitboxDistanceIndex > -1)
+            createMoveOprionsHitboxesByID(hitboxes[hitboxDistanceIndex].getId(), hitboxes[hitboxDistanceIndex].getPosition());
     }
 }
